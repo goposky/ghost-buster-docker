2aa2c14b5f6564e1e549f71124ce6a87b223584d
Gopal Ramachandran <mail@goposky.com>
2017-07-02 19:05:36 +0200

    Update buster

--- buster.sh
+++ buster.sh
@@ -8,10 +8,6 @@ echo "Replacing domainname"
 find static -name index.html -type f -exec sed -i -e 's/localhost\:2368/www.goposky.com/g' {} \;
 find static -name robots.txt -type f -exec sed -i -e 's/localhost\:2368/www.goposky.com/g' {} \;
 
-echo "Adding links header to all pages"
-headervar=$(<header.html)
-find static -name index.html -type f -exec sed -i -e '1s/ˆ*/'"${headervar}"'\'$'\n/' {} \;
-
 if [ "$1" == "--deploy" ]; then
   buster deploy
 fi
